4f18e9d3cc638806e6f90c955170b7e5c563258e
Mohammad Razeghi <moa.raz.ros@proton.me>
2026-01-25 19:55:42 +0100

    Option should just return the inner type

--- src/main/scala/com/melvinlow/json/schema/instances.scala
+++ src/main/scala/com/melvinlow/json/schema/instances.scala
@@ -2,76 +2,48 @@ package com.melvinlow.json.schema
 
 import io.circe.Json
 
-trait instances_low_priority {
-  given intJsonSchemaInstance[T <: Int]: JsonSchemaEncoder[T] with {
+trait instances_low_priority:
+  given intJsonSchemaInstance[T <: Int]: JsonSchemaEncoder[T] with
     def schema: Json = Json.obj("type" -> Json.fromString("integer"))
-  }
 
-  given stringJsonSchemaInstance[T <: String]: JsonSchemaEncoder[T] with {
+  given stringJsonSchemaInstance[T <: String]: JsonSchemaEncoder[T] with
     def schema: Json = Json.obj("type" -> Json.fromString("string"))
-  }
 
-  given longJsonSchemaInstance[T <: Long]: JsonSchemaEncoder[T] with {
+  given longJsonSchemaInstance[T <: Long]: JsonSchemaEncoder[T] with
     def schema: Json = Json.obj("type" -> Json.fromString("integer"))
-  }
 
-  given doubleJsonSchemaInstance[T <: Double]: JsonSchemaEncoder[T] with {
+  given doubleJsonSchemaInstance[T <: Double]: JsonSchemaEncoder[T] with
     def schema: Json = Json.obj("type" -> Json.fromString("number"))
-  }
 
-  given floatJsonSchemaEncoder[T <: Float]: JsonSchemaEncoder[T] with {
+  given floatJsonSchemaEncoder[T <: Float]: JsonSchemaEncoder[T] with
     def schema: Json = Json.obj("type" -> Json.fromString("number"))
-  }
 
-  given booleanJsonSchemaEncoder[T <: Boolean]: JsonSchemaEncoder[T] with {
+  given booleanJsonSchemaEncoder[T <: Boolean]: JsonSchemaEncoder[T] with
     def schema: Json = Json.obj("type" -> Json.fromString("boolean"))
-  }
 
   given listJsonSchemaEncoder[T: JsonSchemaEncoder]: JsonSchemaEncoder[List[T]]
-  with {
+  with
     def schema: Json =
       Json
         .obj(
-          "type"  -> Json.fromString("array"),
+          "type" -> Json.fromString("array"),
           "items" -> JsonSchemaEncoder[T].schema
         )
-  }
 
   given arrayJsonSchemaEncoder[T: JsonSchemaEncoder]
-    : JsonSchemaEncoder[Array[T]]
-  with {
+      : JsonSchemaEncoder[Array[T]] with
     def schema: Json =
       Json
         .obj(
-          "type"  -> Json.fromString("array"),
+          "type" -> Json.fromString("array"),
           "items" -> JsonSchemaEncoder[T].schema
         )
-  }
-}
 
-trait instances extends instances_low_priority {
-  given nullJsonSchemaEncoder: JsonSchemaEncoder[Null] with {
+trait instances extends instances_low_priority:
+  given nullJsonSchemaEncoder: JsonSchemaEncoder[Null] with
     def schema: Json = Json.obj("type" -> Json.fromString("null"))
-  }
-  given optionJsonSchemaEncoder[T: JsonSchemaEncoder]: JsonSchemaEncoder[Option[T]] with {
-    def schema: Json =
-      val innerSchema = JsonSchemaEncoder[T].schema
-      // Extract the type from inner schema, or default to the inner schema structure
-      val innerType = innerSchema.hcursor.downField("type").as[String].getOrElse("object")
-      
-      // Create union type: ["innerType", "null"]
-      Json.obj(
-        "type" -> Json.arr(
-          Json.fromString(innerType),
-          Json.fromString("null")
-        )
-      ).deepMerge(
-        // Merge other properties from inner schema (like description, etc.) but remove the type
-        innerSchema.hcursor.downField("type").delete.top.getOrElse(Json.obj())
-      )
-  }
-}
-
-
+  given optionJsonSchemaEncoder[T: JsonSchemaEncoder]
+      : JsonSchemaEncoder[Option[T]] with
+    def schema: Json = JsonSchemaEncoder[T].schema
 
 object instances extends instances
